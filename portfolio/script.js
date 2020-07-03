605b99f3745a2007d68fe61bfa24df68573cea8f
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-07-03 11:05:27 +0530

    Create a portfolio

---
+++ portfolio/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $("#alchemist_btn").click(function(){
+        $("#alchemist").modal('show');
+    });
+
+    $("#dad_btn").click(function(){
+        $("#dad").modal('show');
+    });
+
+    $("#mind_btn").click(function(){
+        $("#mind").modal('show');
+    });
+
+    $("#monk_btn").click(function(){
+        $("#monk").modal('show');
+    });
+
+    $("#rich_btn").click(function(){
+        $("#rich").modal('show');
+    });
+
+    $("#mockingbird_btn").click(function(){
+        $("#mockingbird").modal('show');
+    });
+});
+
+
+  $(document).ready(function(){
+      $("#paneer_btn").click(function(){
+          $("#paneer").modal('show');
+      });
+
+      $("#chhole_btn").click(function(){
+          $("#chhole").modal('show');
+      });
+
+      $("#dosa_btn").click(function(){
+          $("#dosa").modal('show');
+      });
+
+      $("#biryani_btn").click(function(){
+          $("#biryani").modal('show');
+      });
+
+      $("#noodles_btn").click(function(){
+          $("#noodles").modal('show');
+      });
+
+      $("#pizza_btn").click(function(){
+          $("#pizza").modal('show');
+      });
+  });
+
+ 
+     $(document).ready(function(){
+        $('.dropdown-toggle').dropdown()
+    });
